4b3499314774b7afe0e94ae4df1184b175e38709
Jerry Jäppinen <eiskis@gmail.com>
2017-06-16 23:51:54 +0200

    Split localisation files further

---
+++ src/locales/date-time-formats.js
@@ -0,0 +1,39 @@
+
+// NOTE: browser will handle the final part of localisation, e.g. month names etc.
+// See details: http://www.ecma-international.org/ecma-402/2.0/#sec-intl-datetimeformat-constructor
+export default {
+
+	'en': {
+		short: {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric'
+		},
+		long: {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+			weekday: 'short',
+			hour: 'numeric',
+			minute: 'numeric'
+		}
+	},
+
+	'es': {
+		short: {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric'
+		},
+		long: {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+			weekday: 'short',
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true
+		}
+	}
+
+};
---
+++ src/locales/number-formats.js
@@ -0,0 +1,20 @@
+
+// See details: https://kazupon.github.io/vue-i18n/en/number.html
+export default {
+
+	'en': {
+		currency: {
+			style: 'currency',
+			currency: 'USD'
+		}
+	},
+
+	'es': {
+		currency: {
+			style: 'currency',
+			currency: 'USD',
+			currencyDisplay: 'symbol'
+		}
+	}
+
+};
