988e7ea0187035e54942934e4c7112bc32466242
Jerry Jäppinen <eiskis@gmail.com>
2017-06-20 10:51:19 +0200

    Clearer trimWhitespace test

--- test/unit/specs/util/string.trimWhitespace.spec.js
+++ test/unit/specs/util/string.trimWhitespace.spec.js
@@ -1,78 +1,82 @@
 import { string as stringUtil } from '@util';
 
+// Test cases
+// These will be tested at the start, end and in the middle of the string
+const whitespaceStrings = {
+	space: ' ',
+	spaces: '    ',
+	tab: '\t',
+	tabAndSpace1: ' ' + '\t',
+	tabAndSpace2: ' ' + '\t',
+	tabAndSpace3: ' ' + '\t' + ' ',
+	tabAndMultipleSpaces1: '    ' + '\t',
+	tabAndMultipleSpaces2: '    ' + '\t',
+	tabAndMultipleSpaces3: '    ' + '\t' + '    ',
+	tabAndMultipleSpacesAndTabs1: '    ' + '\t',
+	tabAndMultipleSpacesAndTabs2: '    ' + '\t',
+	tabAndMultipleSpacesAndTabs3: '    ' + '\t' + '    ',
+	newline: '\n',
+	newlines: '\n\n\n'
+};
+
 describe('Util string.trimWhitespace with trailing whitespace', function () {
 
-	const strings = {
-		space: 'Foooo' + ' ',
-		spaces: 'Foooo' + '    ',
-		tab: 'Foooo' + '\t',
-		tabAndSpace1: 'Foooo' + ' ' + '\t',
-		tabAndSpace2: 'Foooo' + ' ' + '\t',
-		tabAndSpace3: 'Foooo' + ' ' + '\t' + ' ',
-		tabAndMultipleSpaces1: 'Foooo' + '    ' + '\t',
-		tabAndMultipleSpaces2: 'Foooo' + '    ' + '\t',
-		tabAndMultipleSpaces3: 'Foooo' + '    ' + '\t' + '    ',
-		tabAndMultipleSpacesAndTabs1: 'Foooo' + '    ' + '\t',
-		tabAndMultipleSpacesAndTabs2: 'Foooo' + '    ' + '\t',
-		tabAndMultipleSpacesAndTabs3: 'Foooo' + '    ' + '\t' + '    ',
-		newline: 'Foooo' + '\n',
-		newlines: 'Foooo' + '\n\n\n'
-	};
+	// Expected result is the same for all these test cases
+	const expectedResult = 'Foooo';
 
-	for (var key in strings) {
-		var string = strings[key];
+	// Test this with each of the test cases provided above
+	for (var key in whitespaceStrings) {
+		var whitespaceString = whitespaceStrings[key];
+
+		// Whitespace is at the end of string
+		var testString = expectedResult + whitespaceString;
+
 		it('should trim ' + key, function () {
-			expect(stringUtil.trimWhitespace(string)).to.equal('Foooo');
+			expect(stringUtil.trimWhitespace(testString)).to.equal(expectedResult);
 		});
+
 	}
 
 });
 
 describe('Util string.trimWhitespace with leading whitespace', function () {
 
-	const strings = {
-		space: ' ' + 'Foooo',
-		spaces: '    ' + 'Foooo',
-		tab: '\t' + 'Foooo',
-		tabAndSpace1: ' ' + '\t' + 'Foooo',
-		tabAndSpace2: ' ' + '\t' + 'Foooo',
-		tabAndSpace3: ' ' + '\t' + ' ' + 'Foooo',
-		tabAndMultipleSpaces1: '    ' + '\t' + 'Foooo',
-		tabAndMultipleSpaces2: '    ' + '\t' + 'Foooo',
-		tabAndMultipleSpaces3: '    ' + '\t' + '    ' + 'Foooo',
-		tabAndMultipleSpacesAndTabs1: '    ' + '\t' + 'Foooo',
-		tabAndMultipleSpacesAndTabs2: '    ' + '\t' + 'Foooo',
-		tabAndMultipleSpacesAndTabs3: '    ' + '\t' + '    ' + 'Foooo',
-		newline: '\n' + 'Foooo',
-		newlines: '\n\n\n' + 'Foooo'
-	};
+	// Expected result is the same for all these test cases
+	const expectedResult = 'Foooo';
 
-	for (var key in strings) {
-		var string = strings[key];
+	// Test this with each of the test cases provided above
+	for (var key in whitespaceStrings) {
+		var whitespaceString = whitespaceStrings[key];
+
+		// Whitespace is at the start of string
+		var testString = whitespaceString + expectedResult;
+
 		it('should trim ' + key, function () {
-			expect(stringUtil.trimWhitespace(string)).to.equal('Foooo');
+			expect(stringUtil.trimWhitespace(testString)).to.equal(expectedResult);
 		});
+
 	}
 
 });
 
 describe('Util string.trimWhitespace with excess whitespace', function () {
 
-	const strings = {
-		space: 'Foo oo',
-		spaces: 'Foo    oo',
-		tab: 'Foo' + '\t' + 'oo',
-		tabs: 'Foo' + '\t\t\t\t' + 'oo',
-		tabsAndSpaces: 'Foo    ' + '\t\t\t\t' + '   oo',
-		newline: 'Foo' + '\n' + 'oo',
-		newlines: 'Foo    ' + '\n\n\n\n' + '   oo'
-	};
+	// Expected result is the same for all these test cases
+	const partOne = 'Foo';
+	const partTwo = 'oo';
+	const expectedResult = partOne + ' ' + partTwo;
 
-	for (var key in strings) {
-		var string = strings[key];
+	// Test this with each of the test cases provided above
+	for (var key in whitespaceStrings) {
+		var whitespaceString = whitespaceStrings[key];
+
+		// Whitespace is injected in the middle
+		var testString = partOne + whitespaceString + partTwo;
+
 		it('should trim ' + key, function () {
-			expect(stringUtil.trimWhitespace(string)).to.equal('Foo oo');
+			expect(stringUtil.trimWhitespace(testString)).to.equal(expectedResult);
 		});
+
 	}
 
 });
