ce82082c3caec1e31cf4439ae19feb4baa1c28b8
Jerry Jäppinen <eiskis@gmail.com>
2017-06-23 14:00:51 +0200

    Removed console logging

--- src/util/events.js
+++ src/util/events.js
@@ -1,11 +1,6 @@
 
 export default {
 
-	setSomeOtherValue: function () {
-		console.log(this.someOtherValue);
-		this.someOtherValue = 'Bar';
-	},
-
 	eventHasMetaKey: function (event) {
 		return (event.ctrlKey || event.metaKey || event.shiftKey);
 	}
