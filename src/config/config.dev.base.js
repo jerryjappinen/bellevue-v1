7004d2467814867f14c681b107b6fe3297ba03d4
Jerry Jäppinen <eiskis@gmail.com>
2017-06-23 12:19:25 +0200

    Documented #28

--- src/config/config.dev.base.js
+++ src/config/config.dev.base.js
@@ -1,11 +1,16 @@
 
-// Override base config values on dev
+// Override base config values when in dev environment
 module.exports = {
 
 	meta: {
 		// title: 'My app DEV'
 	},
 
+	// Turn off caching features for dev
+	//
+	// NOTE
+	// There is a minor issue that might manifest itself if you change this:
+	// https://github.com/Eiskis/bellevue/issues/28
 	offlineCache: {
 		enabled: false
 	},
